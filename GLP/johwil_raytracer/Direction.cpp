7118fd8090e7ac59fd16644a7c89fb32ffc0e3e5
WilmaAxelsson <38458159+WilmaAxelsson@users.noreply.github.com>
2021-09-23 12:03:32 +0200

    Add files via upload

---
+++ GLP/johwil_raytracer/Direction.cpp
@@ -0,0 +1,8 @@
+#include "Direction.h"
+
+
+Direction::Direction(int inX, int inY, int inZ) {
+	x = inX;
+	y = inY;
+	z = inZ;
+}
---
+++ GLP/johwil_raytracer/Direction.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Vertex.h"
+
+class Direction
+{
+private:
+	int x, y, z;
+
+public:
+	Direction(int inX = 0, int inY = 0, int inZ = 0);
+};
+
